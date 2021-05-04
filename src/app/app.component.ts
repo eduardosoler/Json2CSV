699dc94ddc1f93f181c0f0c8a43c82e653c99029
Unknown <du.soler@hotmail.com>
2021-05-03 23:14:50 -0300

    Adicionado validações e labels na interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CSV } from 'src/models/csv';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +10,44 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'json2csv';
 
-  constructor() {
+  form: FormGroup;
 
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      'json': ['', Validators.compose([Validators.required])],
+      'showHeaders': [true],
+      'csv': [{ value: '', disabled: true }]
+    })
+  }
+
+  //Submit do form e gerador de CSV
+  gerar(value: { json: string, showHeaders: boolean }) {
+    if (this.form.valid) {
+      let json: any;
+      try{
+        json = JSON.parse(value.json);
+      }catch(err){
+        console.error(err);
+        this.form.get('json').setErrors({'json': true});
+        return;
+      }
+
+
+      try {
+        let csv = new CSV();
+        if (Array.isArray(json))
+          csv.addArray(json);
+        else
+          csv.addObject(json);
+
+        this.form.get('csv').setValue(csv.toString(value.showHeaders));
+      } catch (err) {
+        console.error(err);
+        this.form.get('json').setErrors({'error': true});
+      }
+    }else{
+      this.form.get('json').markAsTouched();
+      this.form.get('json').markAsDirty();
+    }
   }
 }
